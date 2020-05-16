f78032f9a4d98ff52f78ff25bd32425bda268aff
Om Bikram Rana <ome.bikram@gmail.com>
2020-05-16 17:50:53 +0200

    fetch_data

--- db.js
+++ db.js
@@ -1,24 +1,4 @@
-// const mysql = require('mysql');
-
-// var mysqlConnection = mysql.createConnection({
-//     host:'198.54.115.44', 
-//     user:'avocgocd_sanijew',
-//     password:'Stellar!!11',
-//     database :'avocgocd_sanitest',
-//     multipleStatements: true
-// });
-
-// mysqlConnection.connect((err) => {
-//     if(!err){
-
-//         console.log("connected");
-//     }
-//     else{
-//         console.log("connection failed");
-//     }
-// });
  
-// module.exports = mysqlConnection;   
 var mysql = require('mysql');
 
 var connection = mysql.createPool({
--- frontend/src/components/allproducts.jsx
+++ frontend/src/components/allproducts.jsx
@@ -1,35 +1,57 @@
 import React, { Component } from "react";
-
+import Modal from 'react-modal';
 
 export default class allproducts extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: []
-        };
-  
-        fetch('https://localhost:3000/products') 
-            .then(res => res.json())
-            .then(product => this.setState({ product }));
+            products: []
+        }
     }
-    render() {
+    componentDidMount() {
+     let self =this;
+        fetch(`/products`, {
+            method: 'GET'
+        }).then(function(response){
+            if(response.status >= 400){
+                throw new Error ("Bad response from server");
+            }
+            return response.json();
+        }).then(function(data){
+            self.setState({products: data});
+        }).catch(err => {
+            console.log('caught it!', err);
+        })
+    
+      } 
+      render() {
+         
         return (
-
-            <div className="container-fluid">
-
-                <h2>WelCome To Product Page</h2>
-
-            <ul>
-                {this.state.product.map(products => <li>
-                    <h2>{products.name}</h2>
-                    <p>{products.details}</p>
-                </li>)}
-                </ul>
-
-            </div>
-
  
- 
-        ) 
-    }
+          <div className="container">
+              <div className="panel panel-default p50 uth-panel">
+                  <table className="table table-hover">
+                      <thead>
+                          <tr>
+                            
+                              <th>Name</th>
+                              <th>Price</th>
+                              <th>Details</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          {this.state.products.map(product =>
+                            <tr key={product.id}>
+                                <td>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td>{product.details}</td>
+                            </tr>
+                            )}
+                      </tbody>
+                  </table>
+              </div>
+          </div>
+      );
+      }
+    
 }
