09db3e2d8dd4ae3b768419a5693df931c545f1fc
Om Bikram Rana <ome.bikram@gmail.com>
2020-05-16 19:22:54 +0200

    asdsd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
+
 import allproducts from "./components/allproducts";
 
 class App extends Component {
@@ -10,8 +10,8 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Switch>
-            <Route exact path="/" component={Home} />
-            {/* <Route exact path="/" component={allproducts} /> */}
+        
+            <Route exact path="/" component={allproducts} />
           </Switch>
         </div>
       </BrowserRouter>
